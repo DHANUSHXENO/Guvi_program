81a93ab60d4f75e873448806ef7596094b27a433
2manoj1 <manojmukherjee777@gmail.com>
2015-10-14 23:22:39 +0530

    Update CheckEvenOdd.c

--- CheckEvenOdd.c
+++ CheckEvenOdd.c
@@ -2,5 +2,17 @@
 
 int maim()
 {
+  int n;
+  printf("\n*** Check Even or Odd of given number ***");
+  printf("\n Enter Number: ");
+  scanf("%d",&n);
+  if(n%2==0)
+  {
+    printf("\n Number is Even");
+  }
+  else
+  {
+    printf("\n Number is Odd");
+  }
 return 0;
 }
