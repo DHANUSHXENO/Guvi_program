4a9695b6e4752f6fb7df89616177e37efaa9d980
2manoj1 <manojmukherjee777@gmail.com>
2015-10-14 23:14:00 +0530

    Update CheckNumber.c

--- CheckNumber.c
+++ CheckNumber.c
@@ -1,1 +1,22 @@
+#include<stdio.h>
 
+int main()
+{
+  double n;
+  printf("\n*** Checking Number Positive Negative or Zero***");
+  printf("\n Enter the Number: ");
+  scanf("%lf",&n);
+  if(n>0)
+  {
+    printf("\n %d is Positive Number",n);
+  }
+  else if(n<0)
+  {
+    printf("\n %d is Negative Number",n);
+  }
+  else
+  {
+    printf("\n %d is Zero Number",n);
+  }
+return 0;
+}
